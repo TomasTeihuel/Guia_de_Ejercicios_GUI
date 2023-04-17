0525318d1906fd2f13c343d1a14cc6aafc6d8a65
TomasTeihuel <tomasfelipe.teihuel@alumnos.ulagos.cl>
2023-04-17 12:15:30 -0400

    se agrega codigo

---
+++ guia 17_04_2023/fruta.py
@@ -0,0 +1,46 @@
+from PyQt6.QtWidgets import QApplication, QMainWindow, QWidget, QVBoxLayout, QHBoxLayout, QLineEdit, QPushButton, QLabel
+
+class Ventana(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initUI()
+
+    def initUI(self):
+        # Definir los widgets
+        self.lbl = QLabel('Ingrese una fruta:')
+        self.txtFruta = QLineEdit()
+        self.btnAgregar = QPushButton('Agregar')
+        self.btnAgregar.clicked.connect(self.agregarFruta)
+        self.lblListado = QLabel('Frutas agregadas:')
+        self.txtListado = QLabel()
+
+        # Definir el layout
+        vbox = QVBoxLayout()
+        vbox.addWidget(self.lbl)
+        vbox.addWidget(self.txtFruta)
+        hbox = QHBoxLayout()
+        hbox.addWidget(self.btnAgregar)
+        hbox.addStretch(1)
+        vbox.addLayout(hbox)
+        vbox.addWidget(self.lblListado)
+        vbox.addWidget(self.txtListado)
+
+        # Definir el widget central
+        widget = QWidget()
+        widget.setLayout(vbox)
+        self.setCentralWidget(widget)
+
+        self.frutas = []
+
+    def agregarFruta(self):
+        fruta = self.txtFruta.text()
+        if fruta:
+            self.frutas.append(fruta)
+            self.txtFruta.clear()
+            self.txtListado.setText('\n'.join(self.frutas))
+
+if __name__ == '__main__':
+    app = QApplication([])
+    window = Ventana()
+    window.show()
+    app.exec()
